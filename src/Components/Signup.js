b970b55b192841b67d2f7d4de2fe14b778d24f09
VIVEK KUMAR SONI <119720668+vivekkumarsoni123@users.noreply.github.com>
2025-03-22 22:43:05 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -11,7 +11,7 @@ const Signup = () => {
         e.preventDefault();
         const {name,email,password} = credentials;
     
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`https://user-notebook-2.onrender.com/api/auth/createuser`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
