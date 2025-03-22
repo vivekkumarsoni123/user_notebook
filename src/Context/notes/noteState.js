c3ed9b838bcf392daede060c36c083b24f0c3be5
VIVEK KUMAR SONI <119720668+vivekkumarsoni123@users.noreply.github.com>
2025-03-22 22:44:43 +0530

    Update noteState.js

--- src/Context/notes/noteState.js
+++ src/Context/notes/noteState.js
@@ -3,7 +3,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=> {
-    const host = "http://localhost:5000"; 
+    const host = "https://user-notebook-2.onrender.com/"; 
     const notesInitial = []
     const [notes, setnotes] = useState(notesInitial);
 
