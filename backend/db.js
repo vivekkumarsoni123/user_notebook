768ca6c0129857c5479b362dec138c74f4a25696
VIVEK KUMAR SONI <119720668+vivekkumarsoni123@users.noreply.github.com>
2025-03-22 23:24:56 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-let url="mongodb://localhost:27017/inotebook"
+let url=process.env.MONGO_URL;
 const connecttomongo=async () => {
     try{
         let URL=await mongoose.connect(url)
