b5fca2281a15a2cd37eca373e0af94dc9ec696d6
vivekkumarsoni123 <str.vivekkumarsoni123@gmail.com>
2025-07-27 22:31:21 +0530

    check

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Vivek!')
-// }),
+app.get('/check', (req, res) => {
+  res.send('Hello Vivek!')
+}),
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
